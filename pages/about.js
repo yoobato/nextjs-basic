c4689f0f0ab2a838ba608c1d86e8ed0b4b2a4225
Daeyeol Ryu <dy@yoobato.com>
2022-03-16 02:48:43 +0900

    Add comments (url routing, pre-rendering)

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,6 @@
+// 파일 이름 = URL Path
+// Component 이름은 상관없고, export default를 해야한다.
+
 export default function AboutUs() {
   return "about us";
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
 // ReactJS는 Library (개발자가 React를 사용)
 // NextJS는 Framework (NextJS framework가 개발자의 코드를 호출)
 
+import { useState } from "react";
+
+// NextJS는 Server-side Rendering
+
 export default function Home() {
-  return "hi";
+  // Hydration 예시
+  // cf) Hydration: react.js를 프론트엔드 안에서 실행하는거
+  // NextJS는 react.js를 백엔드에서 렌더링해서 HTML로 만들어 리턴하고, (pre-generated)
+  // 브라우저가 react.js를 로딩하면 NextJS가 렌더링한 것과 연결되어서 react.js 앱이 된다.
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h1>Hello {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+    </div>
+  );
 }

2693f1e67bb4c9a0e96ebfe63621957d97e81720
Daeyeol Ryu <dy@yoobato.com>
2022-03-16 14:39:39 +0900

    CSS Module basic #2 (CSS Module is not recommended to use)

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,15 +6,19 @@ export default function NavBar() {
   const router = useRouter();
 
   return (
-    // CSS Module을 쓰는 경우 className="nav"로 하면 안된다.
-    <nav className={styles.nav}>
+    <nav>
       {/* <a href=""> 태그 대신에 Link 사용 (a 태그는 새로고침 발생) */}
       {/* Link는 결국 a 태그로 바뀌긴 하는데, className이나 style 등을 적용할 수 없음 */}
       <Link href="/">
-        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+        {/* 스타일 두 개 쓰려면 아래처럼 backtick 사용 */}
+        <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>
       </Link>
       <Link href="/about">
-        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>About</a>
+        {/* 스타일 두 개 쓰려면 아래처럼 array join 해도 됨. */}
+        <a className={[
+          styles.link,
+          router.pathname === "/about" ? styles.active : "",
+        ].join(" ")}>About</a>
       </Link>
     </nav>
   );

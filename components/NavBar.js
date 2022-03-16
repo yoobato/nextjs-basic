8d6e52cf48c1d53f40ff9b11cd7d849ece89bed3
Daeyeol Ryu <dy@yoobato.com>
2022-03-16 14:35:50 +0900

    CSS Module basic #1

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
 
   return (
-    <nav>
+    // CSS Module을 쓰는 경우 className="nav"로 하면 안된다.
+    <nav className={styles.nav}>
       {/* <a href=""> 태그 대신에 Link 사용 (a 태그는 새로고침 발생) */}
       {/* Link는 결국 a 태그로 바뀌긴 하는데, className이나 style 등을 적용할 수 없음 */}
       <Link href="/">

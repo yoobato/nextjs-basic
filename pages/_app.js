252b18e561d4a7bad2722a9c07c51a1ccd305a05
Daeyeol Ryu <dy@yoobato.com>
2022-03-19 01:52:23 +0900

    import global css files

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,11 @@
 
 import NavBar from "../components/NavBar";
 
+// global style css는 _app.js에서만 import 할 수 있다.
+// 다른데서 사용 불가능. (*.module.css 형태로 사용해야 함)
+import "../styles/globals.css";
+
+
 // 렌더링하려는 Page가 Component, pageProps로 들어간다.
 export default function App({Component, pageProps}) {
   return (
